3b3e0aebabcf29e97ccc7f72d5bbc4486c22731c
rodrigo.sauda <rodrigo.sauda@hotmail.com>
2023-01-18 18:42:33 +0100

    New Project

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import pandas as pd
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+
+screen.addshape(image)
+turtle.shape(image)
+
+df = pd.read_csv("50_states.csv")
+
+
+while True:
+    screen.update()
+    answer_state = screen.textinput(title="Guess the state", prompt="What`s another state`s name?").capitalize()
+    if answer_state in df['state'].values:
+        cor_x = df.loc[df['state'] == answer_state, 'x']
+        cor_y = df.loc[df['state'] == answer_state, 'y']
+        print(cor_y)
+        print(cor_x)
+
+
+    screen.exitonclick()
 
 
 

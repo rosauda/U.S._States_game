ed36201c8cf85b1f30914dadb5fab4b4e0f778f5
rosauda <rodrigo.sauda@hotmail.com>
2023-01-19 07:12:29 +0100

    play the game

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import pandas as pd
 import turtle
+from turtle import Turtle
+from scoreboard import Scoreboard
 
 screen = turtle.Screen()
+scoreboard = Scoreboard()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
+
 
 screen.addshape(image)
 turtle.shape(image)
 
 df = pd.read_csv("50_states.csv")
+print(df['state'].count())
 
+states_list = []
+missing_states_list = []
 
-while True:
+count = 0
+while count < 50:
     screen.update()
-    answer_state = screen.textinput(title="Guess the state", prompt="What`s another state`s name?").capitalize()
+    answer_state = screen.textinput(title="Guess the state", prompt="What`s another state`s name?").title()
+
+    # If user type "exit" new csv will generated containing all the states that the uses did not guess
+    if answer_state == "Exit":
+        all_states = df.state.to_list()
+        missing_states_list = list(set(all_states).difference(states_list))
+        pd.DataFrame(missing_states_list).to_csv("states_to_learn.csv")
+        break
+
     if answer_state in df['state'].values:
-        cor_x = df.loc[df['state'] == answer_state, 'x']
-        cor_y = df.loc[df['state'] == answer_state, 'y']
-        print(cor_y)
-        print(cor_x)
+        states_list.append(answer_state)
+        if states_list.count(answer_state) > 1:
+            a = 1
+        else:
+            cor_x = int(df.loc[df['state'] == answer_state, 'x'])
+            cor_y = int(df.loc[df['state'] == answer_state, 'y'])
+            state_name = Turtle()
+            state_name.hideturtle()
+            state_name.penup()
+            state_name.goto(cor_x, cor_y)
+            state_name.write(f"{answer_state}")
+            scoreboard.increase_score()
+            count += 1
 
 
-    screen.exitonclick()
+# Find coordinates in the screen by clicking
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
 
 
 
